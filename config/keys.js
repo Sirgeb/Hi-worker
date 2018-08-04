2b925e6f9010b485acde180d3570448880ed01f9
Gabriel Aniora <chybesta@gmail.com>
2018-08-04 21:41:47 +0100

    secure secret keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://gabby:gabito123@ds139082.mlab.com:39082/hi_worker',
-  secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
